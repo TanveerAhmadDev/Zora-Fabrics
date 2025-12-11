c7afa04c3a5c26d54c0d53ad343d36be5643e899
TanveerAhmadDev <tanveeera84@gmail.com>
2025-12-11 10:16:12 +0500

    Multer Fixing

--- backend/middlewares/muler.js
+++ backend/middlewares/muler.js
@@ -1,8 +1,10 @@
 import multer from "multer";
+import path from "path";
+import os from "os";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./temp");
+    cb(null, path.join(os.tmpdir()));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
